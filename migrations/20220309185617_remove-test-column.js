0099613a700e0b29487c415ef5eeba1aa791a350
malerba423 <malerba423@gmail.com>
2022-03-09 13:59:41 -0500

    Create 20220309185617_remove-test-column.js

---
+++ migrations/20220309185617_remove-test-column.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return Promise.all([
+    knex.raw(`      
+      ALTER TABLE foodtruck.items DROP COLUMN just_a_test_column;
+    `),
+  ]);
+};
+
+exports.down = function (knex) {
+  return Promise.all([
+    knex.raw(`
+      ALTER TABLE foodtruck.items ADD COLUMN just_a_test_column TEXT;
+    `),
+  ]);
+};
